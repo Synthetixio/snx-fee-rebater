a04eb7c0a54f914ec8dbfe5d06c7e27aa92f5e3c
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-05-21 17:30:07 -0400

    feat(components): inputs

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,4 +1,14 @@
-import { Flex } from '@chakra-ui/react';
+'use client';
+
+import { SearchIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Select,
+} from '@chakra-ui/react';
 
 import { DataTable } from '~/lib/components/DataTable';
 
@@ -7,12 +17,27 @@ const Home = () => {
     <Flex
       direction="column"
       alignItems="center"
-      justifyContent="center"
       minHeight="70vh"
       gap={4}
       mb={8}
       w="full"
     >
+      <Flex w="100%" gap={8}>
+        <InputGroup bg="black">
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.500" />
+          </InputLeftElement>
+          <Input type="tel" placeholder="Filter by wallet address" />
+        </InputGroup>
+
+        <Box ml="auto" minWidth="240px">
+          <Select bg="black">
+            <option selected value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+        </Box>
+      </Flex>
       <DataTable data={[]} columns={[]} />
     </Flex>
   );
